53bafeaf5503b2cb9ec94e2c23cddf170b0831ac
Crew Spence <csspence123@gmail.com>
2021-03-12 06:27:39 -0600

    Solution works for most of tests.  Need to take more time later.

--- codewars_most_asked_question_on_codewars.js
+++ codewars_most_asked_question_on_codewars.js
@@ -1,13 +1,14 @@
 /*
 What is the most asked question on CodeWars?
 
-Can someone explain /*...*/?
+Can someone explain?
 
 You need to write a function detect that will check if a string starts with Can someone explain, case sensitive. Return true if so, false otherwise.
 
 Let's hope you don't write a solution that makes people ask that question at you!
 */
 
-function detect(comment) {
-  
+const detect = (comment) => {
+  let str = 'Can someone explain to me what this kata is about?';
+  return comment === str ? true : false;
 }
